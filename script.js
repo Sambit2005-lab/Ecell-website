8e52564f1177ed0c348e0531c449da2057d7eb6f
AbhisekSahu009 <abhi2004sahu@gmail.com>
2024-10-23 14:37:35 +0530

    navbar created

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+
+
+  function showSidebar(event) {
+    event.preventDefault(); // Prevent page refresh
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar(event) {
+    event.preventDefault(); // Prevent page refresh
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
